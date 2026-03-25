46c7ef5f65f1e2943047e069424fca485ff0211b
exceptionzofficial <exceptionzofficial@gmail.com>
2026-03-25 10:25:39 +0530

    added api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // Central API Configuration
-// const API_URL = "https://vix-backend.vercel.app/api";
-const API_URL = "https://d416-2405-201-e02c-b031-4c27-3913-63d-7baa.ngrok-free.app/api";
+const API_URL = "https://vix-backend.vercel.app/api";
+// const API_URL = "https://d416-2405-201-e02c-b031-4c27-3913-63d-7baa.ngrok-free.app/api";
 
 export default API_URL;
